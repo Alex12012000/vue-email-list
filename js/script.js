a9543cc0940a3db27487f42f9f849537db33cd63
Alex12012000 <arcex.29@gmail.com>
2022-07-04 14:56:52 +0200

    Bonus

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ var app = new Vue({
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
                     this.emails.push(response.data.response)
-                    console.log(this.emails)
                 })
             }
         }
