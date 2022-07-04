653eb655062f9b40edfaf39a71009feb4deacaea
Alex12012000 <arcex.29@gmail.com>
2022-07-04 14:55:50 +0200

    Finito

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
+      emails: []
     },
-   
+    methods: {
+        apiEmail() {
+            for(let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emails.push(response.data.response)
+                    console.log(this.emails)
+                })
+            }
+        }
+    },
+    mounted: function() {
+        this.apiEmail()
+    }
   })
   
